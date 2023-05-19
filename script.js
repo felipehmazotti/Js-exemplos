af3c62c76972fa04aefd1b3b4faae26714187d6c
felipehmazotti <126676451+felipehmazotti@users.noreply.github.com>
2023-05-19 16:40:48 -0300

    commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// var valor = confirm("Vamos juntos aprender a programar em JS?");
+// if (valor == true) {
+//     alert("Parabéns, vamos lá!!!");
+// } else {
+//     alert("Opcção errada, aperte F5 para recarregar a página.");
+// }
+
+// var email = prompt("Digite seu email");
+// alert("O e-mail "+ email + " foi coletado com sucesso."); 
+// if (valor == true) {
+//     alert("Seu email foi coletado com sucesso")
+// }
