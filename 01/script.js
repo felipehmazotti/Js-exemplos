76b24fa32a83ebe94787dd203ec53d0bda82d03c
felipehmazotti <126676451+felipehmazotti@users.noreply.github.com>
2023-06-14 14:08:22 -0300

    commit

---
+++ 01/script.js
@@ -0,0 +1,28 @@
+// var valor = confirm("Vamos juntos aprender a programar em JS?");
+// if (valor == true) {
+//     alert("Parabéns, vamos lá!!!");
+// } else {
+//     alert("Opcção errada, aperte F5 para recarregar a página.");
+// }
+
+// var email = prompt("Digite seu email");
+//  alert("O e-mail "+ email + " foi coletado com sucesso."); 
+//  if (valor == true) {
+//      alert("Seu email foi coletado com sucesso")
+//  }
+
+// email = prompt("Digite seu email: " );
+
+// if (email == true) {
+//     alert("email não coletado")
+// }else{
+//     alert("email " + email + " foi coletado com sucesso");
+// }
+
+var prova = parseFloat(prompt("Qual a nota da prova?"));
+
+var trabalho = parseFloat(prompt("Qual a nota do trabalho?"));
+
+var notaFinal = prova + trabalho;
+
+alert(notaFinal)
